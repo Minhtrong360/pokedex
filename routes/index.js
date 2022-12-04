e7cc2d909775f621ea99703150dd4bb2282228fb
Minhtrong360 <nguyenminhtrong402@gmail.com>
2022-12-04 17:29:39 +0700

    try catch

--- routes/index.js
+++ routes/index.js
@@ -79,73 +79,79 @@ router.get("/pokemons/:id", function (req, res, next) {
 });
 
 router.post("/pokemons", function (req, res, next) {
-  console.log("first", req);
-  const { name, id, types, url, height, weight, description, abilities } =
-    req.body;
-
-  const newPokemon = {
-    id: id,
-    name: name,
-    types: types,
-    height: height ? height : faker.datatype.number({ max: 100 }),
-    weight: weight ? weight : faker.datatype.number({ max: 100 }),
-    url: url,
-    description: description ? description : faker.company.catchPhrase(),
-    abilities: abilities ? abilities : faker.company.catchPhraseAdjective(),
-  };
-
-  if (!id || !name || !types || !url) {
-    const error = new Error("Missing required data.");
-    error.statusCode = 404;
-    next(error);
-  }
-  if (types.length > 2) {
-    const error = new Error("Pokémon can only have one or two types.");
-    error.statusCode = 404;
-    next(error);
-  }
-
-  const pokemonTypes = [
-    "bug",
-    "dragon",
-    "fairy",
-    "fire",
-    "ghost",
-    "ground",
-    "normal",
-    "psychic",
-    "steel",
-    "dark",
-    "electric",
-    "fighting",
-    "flyingText",
-    "grass",
-    "ice",
-    "poison",
-    "rock",
-    "water",
-  ];
-  if (!types.map((type) => pokemonTypes.includes(type))) {
-    const error = new Error("Pokémon’s type is invalid.");
-    error.statusCode = 404;
-    next(error);
-  }
-  pokemons?.data?.map((pokemon) => {
-    if (id === pokemon.id || name === pokemon.name) {
-      const error = new Error("The Pokémon already exists.");
-      error.statusCode = 401;
+  const { name, id, types, url } = req.body;
+  try {
+    if (!id || !name || !types || !url) {
+      const error = new Error("Missing required data.");
+      error.statusCode = 404;
       next(error);
     }
-    // else {
-    //   // pokemons.data = pokemons.data.push(newPokemon);
-    //   // pokemons.totalPokemons = pokemons.data.length;
-    //   // console.log("pokemons mới", pokemons);
-    //   // fs.writeFileSync(
-    //   //   "./archive/pokemons.json",
-    //   //   JSON.stringify(pokemons.data)
-    //   // );
-    // }
-  });
+    if (types?.length > 2) {
+      const error = new Error("Pokémon can only have one or two types.");
+      error.statusCode = 404;
+      next(error);
+    }
+
+    const pokemonTypes = [
+      "bug",
+      "dragon",
+      "fairy",
+      "fire",
+      "ghost",
+      "ground",
+      "normal",
+      "psychic",
+      "steel",
+      "dark",
+      "electric",
+      "fighting",
+      "flyingText",
+      "grass",
+      "ice",
+      "poison",
+      "rock",
+      "water",
+    ];
+    if (!types?.map((type) => pokemonTypes.includes(type))) {
+      const error = new Error("Pokémon’s type is invalid.");
+      error.statusCode = 404;
+      next(error);
+    }
+
+    let dataPokemons = JSON.parse(fs.readFileSync("./archive/pokemons.json"));
+
+    pokemons?.data?.map((pokemon) => {
+      if (id === pokemon.id || name === pokemon.name) {
+        const error = new Error("The Pokémon already exists.");
+        error.statusCode = 401;
+        next(error);
+      } else {
+        const newPokemon = {
+          id: id,
+          name: name,
+          types: types,
+          height: height ? height : faker.datatype.number({ max: 100 }),
+          weight: weight ? weight : faker.datatype.number({ max: 100 }),
+          url: url,
+          description: description ? description : faker.company.catchPhrase(),
+          abilities: abilities
+            ? abilities
+            : faker.company.catchPhraseAdjective(),
+        };
+        res.status(200).send(newPokemon);
+
+        dataPokemons.data.push(newPokemon);
+        dataPokemons.totalPokemons = dataPokemons.data.length;
+
+        fs.writeFileSync(
+          "./archive/pokemons.json",
+          JSON.stringify(dataPokemons)
+        );
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
